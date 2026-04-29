58d66ef5adb64873543f0adda082460af5e91f8e
Charles McCullough <charles.mccullough@gmail.com>
2026-04-28 21:05:12 -0500

    Create theme and error functions.

---
+++ composeApp/src/commonMain/kotlin/com/charlesmccullough/core/domain/Error.kt
@@ -0,0 +1,4 @@
+package com.charlesmccullough.core.domain
+
+interface Error {
+}
---
+++ composeApp/src/commonMain/kotlin/com/charlesmccullough/theme/CoinRoutineColors.kt
@@ -0,0 +1,36 @@
+package com.charlesmccullough.theme
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.graphics.Color
+
+data class CoinRoutinePalette(
+
+    val profitGreen: Color = Color.Unspecified,
+    val lossRed: Color = Color.Unspecified
+)
+
+
+@Immutable
+data class CoinRoutineColorsPalette(
+    val profitGreen: Color = Color.Unspecified,
+    val lossRed: Color = Color.Unspecified,
+)
+
+val ProfitGreenColor = Color(color = 0xFF32de84)
+val LossRedColor = Color(color = 0xFFD2122E)
+
+val DarkProfitGreenColor = Color(color = 0xFF32de84)
+val DarkLossRedColor = Color(color = 0xFFD2122E)
+
+val LightCoinRoutineColorsPalette = CoinRoutineColorsPalette(
+    profitGreen = ProfitGreenColor,
+    lossRed = LossRedColor,
+)
+
+val DarkCoinRoutineColorsPalette = CoinRoutineColorsPalette(
+    profitGreen = DarkProfitGreenColor,
+    lossRed = DarkLossRedColor,
+)
+
+val LocalCoinRoutineColorsPalette = compositionLocalOf { CoinRoutineColorsPalette() }
